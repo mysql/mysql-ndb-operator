777bd7eeb8afc74a61c24f164c57f0eb86857f22
Bernd Ocklin <bernd.ocklin@oracle.com>
2020-11-13 13:18:10 +0100

    improve config.ini reading

--- pkg/helpers/inifile.go
+++ pkg/helpers/inifile.go
@@ -7,9 +7,12 @@ package helpers
 import (
 	"bufio"
 	"errors"
+	"io"
 	"os"
 	"strings"
 )
+
+const headerSection = "header"
 
 /* config variable name, config value pair */
 type Section map[string]string
@@ -28,13 +31,45 @@ func NewConfig() *ConfigIni {
 	}
 }
 
-/*
-	parses an ini configuration file and returns it
-	as a config struct
-*/
-func parseFile(file string) (*ConfigIni, error) {
+func GetValueFromSingleSectionGroup(c *ConfigIni, sectionName string, key string) string {
 
-	c := NewConfig()
+	if grp, ok := c.Groups[sectionName]; ok {
+		if len(grp) > 0 {
+			if value, exists := grp[0][key]; exists {
+				return value
+			}
+		}
+	}
+
+	return ""
+}
+
+/* ensures a new group if not exists and a new section within that group */
+func (c *ConfigIni) addSection(sectionName string) *Section {
+
+	grp := []Section{}
+	if c.Groups[sectionName] == nil {
+		// new group
+		c.Groups[sectionName] = grp
+	} else {
+		grp = c.Groups[sectionName]
+	}
+
+	if sectionName == headerSection {
+		// if there is a header section it should only have 1 element and we return that
+		if len(grp) > 0 {
+			return &grp[0]
+		}
+	}
+
+	// create a new section under the new or existing group
+	currentSection := &Section{}
+	c.Groups[sectionName] = append(c.Groups[sectionName], *currentSection)
+
+	return currentSection
+}
+
+func ParseFile(file string) (*ConfigIni, error) {
 
 	f, err := os.Open(file)
 	if err != nil {
@@ -43,14 +78,59 @@ func parseFile(file string) (*ConfigIni, error) {
 	defer f.Close()
 
 	reader := bufio.NewReader(f)
+
+	return parseConfig(reader)
+}
+
+func ParseString(inString string) (*ConfigIni, error) {
+
+	reader := bufio.NewReader(strings.NewReader(inString))
+
+	return parseConfig(reader)
+}
+
+/*
+	parses an ini configuration file and returns it
+	as a config struct
+
+	grp              e.g. [ndbd]
+		section        e.g. [ndbd]
+			key=value
+			key=value
+			...
+		section        e.g. [ndbd]
+	grp
+		...
+
+	all sections [section name] of same kind will be grouped under same name
+
+	each grp is just a map of sections pointing to an array of sections:
+		map[section name] = []sections
+
+	each section has unique keys with a value:
+	  map[key] = value
+*/
+func parseConfig(reader *bufio.Reader) (*ConfigIni, error) {
+
+	c := NewConfig()
+
 	lineno := 1
 	sectionName := ""
+	seenHeader := false // we only want to allow 1 header and keep track
+	isComment := false
+
 	var currentSection *Section = nil
 
 	for {
 
-		line := ""
-		line, err = reader.ReadString('\n')
+		line, err := reader.ReadString('\n')
+
+		// don't exit on io.EOF if there is still something read into line
+		// that is e.g. the case when there is no newline at end of last line
+		if err != nil && err != io.EOF {
+			return c, err
+		}
+
 		if len(line) == 0 {
 			break
 		}
@@ -60,24 +140,36 @@ func parseFile(file string) (*ConfigIni, error) {
 		}
 
 		if line[0] == ';' || line[0] == '#' {
-			continue
+			line = line[1:]
+			line = strings.TrimSpace(line)
+			if len(line) == 0 {
+				continue
+			}
+
+			// for all key value pairs in a comment we collect that under "header" section
+			if !isComment {
+				currentSection = c.addSection(headerSection)
+				isComment = true
+			}
+		} else {
+			// reset comment
+			if isComment {
+				seenHeader = true
+			}
+			isComment = false
 		}
 
 		if line[0] == '[' {
+			if isComment {
+				// no section headers in comments - only values
+				continue
+			}
 			if line[len(line)-1] != ']' {
 				return nil, errors.New("Incomplete section name in line " + string(lineno) + " " + line)
 			}
+
 			sectionName = string(line[1 : len(line)-1])
-
-			if c.Groups[sectionName] == nil {
-				// new group
-				grp := []Section{}
-				c.Groups[sectionName] = grp
-			}
-
-			currentSection = &Section{}
-			c.Groups[sectionName] = append(c.Groups[sectionName], *currentSection)
-
+			currentSection = c.addSection(sectionName)
 			continue
 		}
 
@@ -85,15 +177,21 @@ func parseFile(file string) (*ConfigIni, error) {
 			return nil, errors.New("Non-empty line without section" + string(lineno) + " " + line)
 		}
 
-		split := strings.Split(line, "=")
+		split := strings.SplitN(line, "=", 2)
 		if len(split) != 2 {
+			if isComment {
+				continue
+			}
 			return nil, errors.New("Format error " + string(lineno) + " " + line)
 		}
 
+		if isComment && seenHeader {
+			// ignore config values in comments outside header section
+			continue
+		}
 		(*currentSection)[split[0]] = split[1]
 
 		lineno++
-
 	}
 
 	return c, nil
