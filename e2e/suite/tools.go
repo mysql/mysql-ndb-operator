1cb10a252cfc9d8f27e3fca77457cbccb17f6204
Bernd Ocklin <bernd.ocklin@oracle.com>
2021-02-01 08:01:55 +0100

    Add and use tooling to create resources directly from yaml file.
    - in order to prepare tests it comes in handy
      to create CRD, Deployments, etc. from a
      yaml file

--- e2e/suite/tools.go
+++ e2e/suite/tools.go
@@ -87,3 +87,47 @@ func ReplaceAllProperties(content string, key string, newValue string) (string, 
 
 	return result, nil
 }
+
+func createOrDeleteFromYaml(ns string, path string, filename string, command string) {
+
+	if command != "delete" && command != "create" {
+		return
+	}
+
+	y := YamlFile(path, filename)
+
+	if ns != "" {
+		var err error
+		y, err = ReplaceAllProperties(y, "namespace", ns)
+		if err != nil {
+			klog.Fatalf("Error parsing %s\n", y)
+		}
+	}
+
+	framework.RunKubectlOrDieInput(ns, y, command, "-n", ns, "-f", "-")
+}
+
+// CreateFromYaml creates a resource from a yaml file
+// in a specfic namespace and with kubectl
+func CreateFromYaml(ns string, path string, filename string) {
+	createOrDeleteFromYaml(ns, path, filename, "create")
+}
+
+// CreateFromYamls creates a resources from resource paths given in an array
+func CreateFromYamls(ns string, resourceFiles []string) {
+	for _, d := range resourceFiles {
+		CreateFromYaml(ns, "", d)
+	}
+}
+
+// DeleteFromYaml deletes resources given in a yaml file
+func DeleteFromYaml(ns string, path string, filename string) {
+	createOrDeleteFromYaml(ns, path, filename, "delete")
+}
+
+// DeleteFromYamls deletes resources from resource paths given in an array
+func DeleteFromYamls(ns string, resourceFiles []string) {
+	for _, d := range resourceFiles {
+		DeleteFromYaml(ns, "", d)
+	}
+}
