ccd3ec9ffffbd7cf444678f44f910760c18a8d1a
Bernd Ocklin <bernd.ocklin@oracle.com>
2021-02-04 14:40:56 +0100

    controller - add a check if Ndb status needs to be updated
    - Ndb status was updated every sync() run without any action actually taken

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -1201,16 +1201,17 @@ func patchPod(kubeClient kubernetes.Interface, oldData *corev1.Pod, newData *cor
 
 func (sc *SyncContext) updateNdbStatus() error {
 
-	// NEVER modify objects from the store. It's a read-only, local cache.
-	// You can use DeepCopy() to make a deep copy of original object and modify this copy
-	// Or create a copy manually for better performance
-
-	// we already received a copy here
+	// we already received a deep copy here
 	ndb := sc.ndb
+
+	if ndb.Status.ProcessedGeneration == ndb.ObjectMeta.Generation {
+		return nil
+	}
 
 	updateErr := wait.ExponentialBackoff(retry.DefaultBackoff, func() (ok bool, err error) {
 
-		klog.Infof("Updating ndb cluster status")
+		klog.Infof("Updating ndb cluster status: from process gen %d to %d",
+			ndb.Status.ProcessedGeneration, ndb.ObjectMeta.Generation)
 
 		ndb.Status.LastUpdate = metav1.NewTime(time.Now())
 		ndb.Status.ProcessedGeneration = ndb.ObjectMeta.Generation
