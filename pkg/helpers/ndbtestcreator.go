decd7bd750317991fe89bdc54f180de35a6c4bbf
Bernd Ocklin <bernd.ocklin@oracle.com>
2021-02-10 16:01:29 +0100

    Move creation of Ndb resource for testing into separate file
    - allows it to be used in more tests

---
+++ pkg/helpers/ndbtestcreator.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+	ndbcontroller "github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// NewTestNdb creates a new Ndb resource for testing
+func NewTestNdb(namespace string, name string, noofnodes int) *ndbcontroller.Ndb {
+	return &ndbcontroller.Ndb{
+		TypeMeta: metav1.TypeMeta{APIVersion: ndbcontroller.SchemeGroupVersion.String()},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: ndbcontroller.NdbSpec{
+			NodeCount:       IntToInt32Ptr(int(noofnodes)),
+			RedundancyLevel: IntToInt32Ptr(int(2)),
+			Mysqld: ndbcontroller.NdbMysqldSpec{
+				NodeCount: IntToInt32Ptr(int(noofnodes)),
+			},
+		},
+	}
+}
