96e2a7a822fdc1b2e88f1c1afe62508c845c8ee6
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2021-03-09 14:13:21 +0530

    Added/Updated testcases for webhooks
    Change-Id: I914692f67b73c5cebe3ff442e7f02d1ba4f8b887

---
+++ pkg/webhook/certificate_generator_test.go
@@ -0,0 +1,42 @@
+package webhook
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+)
+
+func verifyHostname(t *testing.T, cert *x509.Certificate, hostname string) {
+	t.Helper()
+	if err := cert.VerifyHostname(hostname); err != nil {
+		t.Fatal("Failed to verify hostname : ", err)
+	}
+}
+
+func Test_createCertificate(t *testing.T) {
+	// create the certificate and use it to verify itself
+	td := createCertificate("test-service", "test-ns")
+
+	if td == nil || td.certificate == nil || td.privateKey == nil {
+		t.Fatal("newSelfSignedCertificate failed to create a new certificate")
+	}
+
+	// try creating a certificate
+	_, err := tls.X509KeyPair(td.certificate, td.privateKey)
+	if err != nil {
+		t.Fatal("Failed to create certificate : ", err)
+	}
+
+	// verify that the certificate has the correct DNS names
+	block, _ := pem.Decode(td.certificate)
+	if block == nil {
+		t.Fatalf("Failed to decode the generated certificate")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		t.Fatal("Failed to parse the generated certificate : ", err)
+	}
+	verifyHostname(t, cert, "test-service.test-ns.svc")
+	verifyHostname(t, cert, "test-service")
+}
