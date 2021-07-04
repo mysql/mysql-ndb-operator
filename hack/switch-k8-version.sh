13315aaaa31512b49b43c75d3a30a2b9845cf5cf
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2021-07-05 02:00:11 +0530

    Updated License
    Changes :
     - Updated the copyright year on files with existing copyright header.
     - Added the copyright header to the files where it was missing.
    
    Change-Id: I2becbd6a18fef3bd5eae3c895cdcaf7d89f72de5

--- hack/switch-k8-version.sh
+++ hack/switch-k8-version.sh
@@ -1,5 +1,9 @@
 # https://github.com/kubernetes/kubernetes/issues/79384#issuecomment-521493597
 
+# Copyright (c) 2021, Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
 #!/bin/sh
 set -euo pipefail
 
--- pkg/helpers/scripts/ndbcluster-healthcheck.sh
+++ pkg/helpers/scripts/ndbcluster-healthcheck.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright (c) 2021, Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
 # Health check script - used by the health probes of the MySQL Server image
 
 set -o errexit
