2cd6363f5db907bca58d3e335fca1e1680c6c692
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2021-08-16 02:37:58 +0530

    Autodetect MySQL Server root password in e2e tests
    Change-Id: Ib43166d21a98df11aa4d387fb53d1bb5598b803c

--- e2e-tests/utils/mysql/wrapper.go
+++ e2e-tests/utils/mysql/wrapper.go
@@ -5,10 +5,14 @@
 package mysql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"github.com/mysql/ndb-operator/e2e-tests/utils/secret"
 	"github.com/mysql/ndb-operator/e2e-tests/utils/service"
+	"github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1alpha1"
 	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"
 	"time"
@@ -17,14 +21,16 @@ import (
 )
 
 // Connect extracts the ip address of the MySQL Load balancer service and creates a connection to it
-func Connect(clientset kubernetes.Interface, namespace string, ndbName string, dbname string) *sql.DB {
+func Connect(clientset kubernetes.Interface, nc *v1alpha1.NdbCluster, dbname string) *sql.DB {
+
+	gomega.Expect(nc.GetMySQLServerNodeCount()).NotTo(
+		gomega.BeZero(), fmt.Sprintf("No MySQL Servers configured for NdbCluster %q", nc.Name))
 
 	ginkgo.By("connecting to the MySQL Load balancer")
-	serviceName := ndbName + "-mysqld-ext"
-	host, port := service.GetServiceAddressAndPort(clientset, namespace, serviceName)
+	serviceName := nc.GetServiceName("mysqld") + "-ext"
+	host, port := service.GetServiceAddressAndPort(clientset, nc.Namespace, serviceName)
 	user := "root"
-	// TODO: Auto detect password from Ndb object
-	password := "ndbpass"
+	password := secret.GetMySQLRootPassword(context.TODO(), clientset, nc)
 	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?timeout=10s", user, password, host, port, dbname)
 	db, err := sql.Open("mysql", dataSource)
 	framework.ExpectNoError(err)
