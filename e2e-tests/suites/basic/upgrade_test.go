e05b4232246fdbb6005c5794a8b1edeb5177c60e
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2022-07-07 17:27:37 +0530

    MYSQLCLUSTER-5148 Support MySQL Cluster online upgrade
    MySQL Cluster online upgrades are already supported by the NDB Operator
    after the changes made by MYSQLCLUSTER-5660. This patch just adds a
    testcase to verify the feature.
    
    Change-Id: I2106c6f76bc71abf1d88ef27422fa882e5b0b5f0

---
+++ e2e-tests/suites/basic/upgrade_test.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+//
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+package e2e
+
+import (
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	"strings"
+
+	"github.com/mysql/ndb-operator/e2e-tests/utils/mgmapi"
+	"github.com/mysql/ndb-operator/e2e-tests/utils/ndbtest"
+	"github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1alpha1"
+	"github.com/mysql/ndb-operator/pkg/helpers/testutils"
+
+	"k8s.io/client-go/kubernetes"
+)
+
+// expectMySQLClusterVersion verifies if all the nodes in the MySQL Cluster
+// run the version specified in the NdbCluster resource's image value.
+func expectMySQLClusterVersion(clientset kubernetes.Interface, nc *v1alpha1.NdbCluster) {
+	mgmClient := mgmapiutils.ConnectToMgmd(clientset, nc)
+	status, err := mgmClient.GetStatus()
+	gomega.Expect(err).ShouldNot(gomega.HaveOccurred())
+	expectedVersion := strings.Split(nc.Spec.Image, ":")[1]
+	for _, node := range status {
+		if node.IsConnected {
+			gomega.Expect(node.SoftwareVersion).To(gomega.Equal(expectedVersion))
+		}
+	}
+}
+
+var _ = ndbtest.NewOrderedTestCase("MySQL Cluster minor upgrade", func(tc *ndbtest.TestContext) {
+	var ns string
+	var c kubernetes.Interface
+	var testNdb *v1alpha1.NdbCluster
+
+	ginkgo.BeforeAll(func() {
+		ginkgo.By("extracting values from TestContext")
+		ns = tc.Namespace()
+		c = tc.K8sClientset()
+		// Create a new NdbCluster resource to be used by the test
+		testNdb = testutils.NewTestNdb(ns, "upgrade-test", 2)
+
+		ginkgo.DeferCleanup(func() {
+			ginkgo.By("Delete the NdbCluster resource")
+			ndbtest.KubectlDeleteNdbObj(c, testNdb)
+		})
+	})
+
+	ginkgo.When("NdbCluster is created with a old MySQL Cluster image", func() {
+		ginkgo.BeforeAll(func() {
+			testNdb.Spec.Image = "mysql/mysql-cluster:8.0.28"
+			ndbtest.KubectlApplyNdbObj(c, testNdb)
+		})
+
+		ginkgo.It("should start all the MySQL Cluster nodes with the version specified in the image", func() {
+			expectMySQLClusterVersion(c, testNdb)
+		})
+	})
+
+	ginkgo.When("NdbCluster is updated with a newer MySQL Cluster image", func() {
+		ginkgo.BeforeAll(func() {
+			testNdb.Spec.Image = "mysql/mysql-cluster:8.0.29"
+			ndbtest.KubectlApplyNdbObj(c, testNdb)
+		})
+
+		ginkgo.It("should have upgraded all the MySQL Cluster nodes to the version specified in the image", func() {
+			expectMySQLClusterVersion(c, testNdb)
+		})
+	})
+})
