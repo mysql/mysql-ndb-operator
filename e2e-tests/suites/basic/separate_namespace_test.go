7e7620ce8f9e882f30c2f82a3746b83e3c2f342e
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2022-07-29 12:00:21 +0530

    MYSQLCLUSTER-5878 Incorrect host for ndb-operator mysql user
    Fixed the hostname of the ndb-operator-user mysql account.
    
    Change-Id: I56d84f6a9eb2b427eafbfbaadf3a3ec2511dd7a8

---
+++ e2e-tests/suites/basic/separate_namespace_test.go
@@ -0,0 +1,61 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+//
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+package e2e
+
+import (
+	"context"
+
+	"github.com/mysql/ndb-operator/e2e-tests/utils/k8sutils"
+	"github.com/mysql/ndb-operator/e2e-tests/utils/ndbtest"
+	"github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1alpha1"
+	"github.com/mysql/ndb-operator/pkg/constants"
+	"github.com/mysql/ndb-operator/pkg/helpers/testutils"
+
+	sfset_utils "github.com/mysql/ndb-operator/e2e-tests/utils/statefulset"
+
+	"github.com/onsi/ginkgo/v2"
+	clientset "k8s.io/client-go/kubernetes"
+)
+
+var _ = ndbtest.NewOrderedTestCase("NdbCluster in a separate namespace", func(tc *ndbtest.TestContext) {
+	var ctx context.Context
+	var ns, ndbNamespace string
+	var c clientset.Interface
+	var testNdb *v1alpha1.NdbCluster
+
+	ginkgo.BeforeEach(func() {
+		ginkgo.By("extracting values from TestContext")
+		ns = tc.Namespace()
+		c = tc.K8sClientset()
+		ctx = tc.Ctx()
+
+		ndbNamespace = ns + "-deux"
+		testNdb = testutils.NewTestNdb(ndbNamespace, "separate-namespace-test", 2)
+	})
+
+	ginkgo.When("the NdbCluster resource is created in a namespace different than that of the operator", func() {
+
+		ginkgo.BeforeEach(func() {
+			// Create the namespace for the test
+			_, err := k8sutils.CreateNamespace(ctx, c, ndbNamespace)
+			ndbtest.ExpectNoError(err, "create namespace %q failed", ndbNamespace)
+			ginkgo.DeferCleanup(func() {
+				err := k8sutils.DeleteNamespace(ctx, c, ndbNamespace)
+				ndbtest.ExpectNoError(err, "delete namespace %q failed", ndbNamespace)
+			})
+		})
+
+		ginkgo.AfterEach(func() {
+			ndbtest.KubectlDeleteNdbObj(c, testNdb)
+		})
+
+		ginkgo.It("should deploy MySQL cluster nodes in the specified namespace", func() {
+			ndbtest.KubectlApplyNdbObj(c, testNdb)
+			sfset_utils.ExpectHasReplicas(c, ndbNamespace, testNdb.GetWorkloadName(constants.NdbNodeTypeMgmd), 2)
+			sfset_utils.ExpectHasReplicas(c, ndbNamespace, testNdb.GetWorkloadName(constants.NdbNodeTypeNdbmtd), 2)
+			sfset_utils.ExpectHasReplicas(c, ndbNamespace, testNdb.GetWorkloadName(constants.NdbNodeTypeMySQLD), 2)
+		})
+	})
+})
