656a10a0be6116a70fd883a7fceab1894c4eeed5
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2022-08-15 23:09:27 +0530

    MYSQLCLUSTER-5951 Ensure atleast one MySQL Server is always run [1/3]
    Updated webhook validator
    
    Updated the validator interface into a more generic admission
    controller interface that can also support mutate requests in the future.
    
    Change-Id: Icaa274da21fdf9dc436554c0b89a170a7f750a20

--- pkg/webhook/admission_controller_interface.go
+++ pkg/webhook/admission_controller_interface.go
@@ -6,54 +6,56 @@ package webhook
 
 import (
 	"fmt"
-	"k8s.io/apimachinery/pkg/types"
 	"regexp"
 
-	v1 "k8s.io/api/admission/v1"
+	admissionv1 "k8s.io/api/admission/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/klog/v2"
 )
 
-type validator interface {
+type admissionController interface {
 	getGVR() *metav1.GroupVersionResource
 	getGVK() *schema.GroupVersionKind
 	newObject() runtime.Object
 	// validate functions should validate the request and return a AdmissionResponse
-	validateCreate(reqUID types.UID, obj runtime.Object) *v1.AdmissionResponse
-	validateUpdate(reqUID types.UID, obj runtime.Object, oldObj runtime.Object) *v1.AdmissionResponse
+	validateCreate(reqUID types.UID, obj runtime.Object) *admissionv1.AdmissionResponse
+	validateUpdate(reqUID types.UID, obj runtime.Object, oldObj runtime.Object) *admissionv1.AdmissionResponse
 }
 
-func unsupportedOperation(reqUID types.UID, operation v1.Operation) *v1.AdmissionResponse {
+func unsupportedValidatorOperation(reqUID types.UID, operation admissionv1.Operation) *admissionv1.AdmissionResponse {
 	errMsg := fmt.Sprintf("validating a %s operation not supported", operation)
 	klog.Error(errMsg)
 	return requestDeniedBad(reqUID, errMsg)
 }
 
-func validate(req *v1.AdmissionRequest, v validator) *v1.AdmissionResponse {
+type requestExecutor func(req *admissionv1.AdmissionRequest, ac admissionController) *admissionv1.AdmissionResponse
+
+func validate(req *admissionv1.AdmissionRequest, ac admissionController) *admissionv1.AdmissionResponse {
 	// Verify right resource is passed
-	resource := v.getGVR()
+	resource := ac.getGVR()
 	if req.Resource != *resource {
 		errMsg := fmt.Sprintf("expected resource %v but got %v", *resource, req.Resource)
 		return requestDeniedBad(req.UID, errMsg)
 	}
 
 	// Handle operation
-	defaultGVK := v.getGVK()
+	defaultGVK := ac.getGVK()
 	decoder := scheme.Codecs.UniversalDeserializer()
 	switch req.Operation {
-	case v1.Create:
+	case admissionv1.Create:
 		// retrieve new object and validate it
-		obj, _, err := decoder.Decode(req.Object.Raw, defaultGVK, v.newObject())
+		obj, _, err := decoder.Decode(req.Object.Raw, defaultGVK, ac.newObject())
 		if err != nil {
 			return requestDeniedBad(req.UID, err.Error())
 		}
 		klog.V(5).Info(fmt.Sprintf("Retrieved new object : %v", obj))
-		return v.validateCreate(req.UID, obj)
+		return ac.validateCreate(req.UID, obj)
 
-	case v1.Update:
+	case admissionv1.Update:
 		// any updates made from the ndb-operator can be accepted without validation
 		if updateFromNdbOperator, _ := regexp.MatchString(
 			"system:serviceaccount:.*:ndb-operator", req.UserInfo.Username); updateFromNdbOperator {
@@ -62,22 +64,22 @@ func validate(req *v1.AdmissionRequest, v validator) *v1.AdmissionResponse {
 		}
 
 		// retrieve new and old objects
-		obj, _, err := decoder.Decode(req.Object.Raw, defaultGVK, v.newObject())
+		obj, _, err := decoder.Decode(req.Object.Raw, defaultGVK, ac.newObject())
 		if err != nil {
 			return requestDeniedBad(req.UID, err.Error())
 		}
 		klog.V(5).Info(fmt.Sprintf("Retrieved new object : %v", obj))
 
-		oldObject, _, err := decoder.Decode(req.OldObject.Raw, defaultGVK, v.newObject())
+		oldObject, _, err := decoder.Decode(req.OldObject.Raw, defaultGVK, ac.newObject())
 		if err != nil {
 			return requestDeniedBad(req.UID, err.Error())
 		}
 		klog.V(5).Info(fmt.Sprintf("Retrieved old object : %v", oldObject))
 
 		// validate the update
-		return v.validateUpdate(req.UID, obj, oldObject)
+		return ac.validateUpdate(req.UID, obj, oldObject)
 
 	default:
-		return unsupportedOperation(req.UID, req.Operation)
+		return unsupportedValidatorOperation(req.UID, req.Operation)
 	}
 }
