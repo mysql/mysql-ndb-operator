b5f926ea4284861f80a436bd6850a870a8296cff
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2022-08-16 01:52:13 +0530

    MYSQLCLUSTER-5951 Ensure atleast one MySQL Server is always run [3/3]
    Updated the ndbcluster mutate method to ensure that atleast one MySQL
    Server is defined as the part of NdbCluster spec. If not, a patch is
    generated and returned to add one MySQL Cluster to the spec.
    
    Change-Id: Ie0215953fd9640ede6688593b8b1929f8a1892aa

--- e2e-tests/suites/mysql/mutable_root_host_test.go
+++ e2e-tests/suites/mysql/mutable_root_host_test.go
@@ -6,12 +6,7 @@ package e2e
 
 import (
 	"context"
-
-	"github.com/onsi/ginkgo/v2"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
-	clientset "k8s.io/client-go/kubernetes"
+	"fmt"
 
 	"github.com/mysql/ndb-operator/e2e-tests/utils/k8sutils"
 	"github.com/mysql/ndb-operator/e2e-tests/utils/ndbtest"
@@ -20,40 +15,20 @@ import (
 	"github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1alpha1"
 	"github.com/mysql/ndb-operator/pkg/constants"
 	"github.com/mysql/ndb-operator/pkg/helpers/testutils"
+	"github.com/onsi/ginkgo/v2"
+	clientset "k8s.io/client-go/kubernetes"
 )
-
-// Helper function to build a set of bash commands that is executed in pod/exec
-func buildExecCmd(ctx context.Context, c clientset.Interface, testNdb *v1alpha1.NdbCluster, ns string) []string {
-	//extract password from secret
-	password := secretutils.GetMySQLRootPassword(ctx, c, testNdb)
-	password = "-p" + password
-	ndbName := testNdb.Name
-
-	//extract hostname from mysql pod
-	Selector := labels.Set{constants.ClusterNodeTypeLabel: "mysqld", constants.ClusterLabel: ndbName}.AsSelector()
-	mysqlpodlist, _ := c.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{
-		LabelSelector: Selector.String(),
-	})
-	hostname := "-h" + mysqlpodlist.Items[1].Status.PodIP
-
-	//Build query string
-	query := "-e\"use mysql;select user, host from mysql.user;\""
-
-	cmd := []string{
-		"sh",
-		"-c",
-		"mysql -uroot " + password + " " + hostname + " " + query,
-	}
-	return cmd
-}
 
 var _ = ndbtest.NewOrderedTestCase("Mutable root host", func(tc *ndbtest.TestContext) {
 	var ns string
 	var c clientset.Interface
 	var testNdb *v1alpha1.NdbCluster
 	var ctx context.Context
-	var mysqlpodlist *v1.PodList
 	var ndbName string
+
+	var mysqlPod0Name, mysqlPod1Name, mysqlPod0HostName string
+
+	var mysqlCmd []string
 
 	ginkgo.BeforeAll(func() {
 		ginkgo.By("extracting values from TestContext")
@@ -65,12 +40,31 @@ var _ = ndbtest.NewOrderedTestCase("Mutable root host", func(tc *ndbtest.TestCon
 		ndbName = "mutable-roothost-test"
 		testNdb = testutils.NewTestNdb(ns, ndbName, 2)
 
+		// create the secret in K8s
+		mysqlRootSecretName := ndbName + "-root-secret"
+		secretutils.CreateSecretForMySQLRootAccount(ctx, c, mysqlRootSecretName, ns)
+		testNdb.Spec.Mysqld.RootPasswordSecretName = mysqlRootSecretName
+
+		mysqlPod0Name = testNdb.GetWorkloadName(constants.NdbNodeTypeMySQLD) + "-0"
+		mysqlPod1Name = testNdb.GetWorkloadName(constants.NdbNodeTypeMySQLD) + "-1"
+
+		mysqlPod0HostName = fmt.Sprintf("%s-0.%s.%s",
+			testNdb.GetWorkloadName(constants.NdbNodeTypeMySQLD),
+			testNdb.GetServiceName(constants.NdbNodeTypeMySQLD), testNdb.Namespace)
+
+		mysqlCmd = []string{
+			"sh",
+			"-c",
+			"mysql --user=root --password=" + secretutils.TestRootPassword +
+				" --protocol=tcp --host=" + mysqlPod0HostName +
+				" --database=mysql --execute='select user, host from user;'",
+		}
+
 		// Setup cleanup methods
 		ginkgo.DeferCleanup(func() {
 			// common cleanup for all specs cleanup
 			ndbtest.KubectlDeleteNdbObj(c, testNdb)
 		})
-
 	})
 
 	ginkgo.JustBeforeEach(ginkgo.OncePerOrdered, func() {
@@ -78,13 +72,6 @@ var _ = ndbtest.NewOrderedTestCase("Mutable root host", func(tc *ndbtest.TestCon
 		// validate the status updates made by the operator during the sync
 		ndbutils.ValidateNdbClusterStatusUpdatesDuringSync(
 			ctx, tc.NdbClientset(), ns, ndbName)
-
-		Selector := labels.Set{constants.ClusterNodeTypeLabel: "mysqld", constants.ClusterLabel: ndbName}.AsSelector()
-		if mysqlpodlist == nil {
-			mysqlpodlist, _ = c.CoreV1().Pods(ns).List(ctx, metav1.ListOptions{
-				LabelSelector: Selector.String(),
-			})
-		}
 	})
 
 	// Due to Bug #33904288: host change from % -> any address is not getting distributed
@@ -98,12 +85,10 @@ var _ = ndbtest.NewOrderedTestCase("Mutable root host", func(tc *ndbtest.TestCon
 		})
 
 		ginkgo.It("ensure that root user is not accessible from any of the mysql pod in cluster", func() {
-			cmd := buildExecCmd(ctx, c, testNdb, ns)
-
-			_, _, err := k8sutils.Exec(c, mysqlpodlist.Items[0].Name, ns, cmd)
+			_, _, err := k8sutils.Exec(c, mysqlPod0Name, ns, mysqlCmd)
 			ndbtest.ExpectError(err, "Expecting failure but succeed")
 
-			_, _, err = k8sutils.Exec(c, mysqlpodlist.Items[1].Name, ns, cmd)
+			_, _, err = k8sutils.Exec(c, mysqlPod1Name, ns, mysqlCmd)
 			ndbtest.ExpectError(err, "Expecting failure but succeed")
 		})
 	})
@@ -112,16 +97,14 @@ var _ = ndbtest.NewOrderedTestCase("Mutable root host", func(tc *ndbtest.TestCon
 	ginkgo.When("Root host in MySQL Server is assigned to ip of one of the mysql pod in cluster", func() {
 
 		ginkgo.BeforeAll(func() {
-			testNdb.Spec.Mysqld.RootHost = mysqlpodlist.Items[0].Status.PodIP
+			testNdb.Spec.Mysqld.RootHost = mysqlPod0HostName + ".%"
 		})
 
 		ginkgo.It("ensure that root user is accessible from only one pod", func() {
-			cmd := buildExecCmd(ctx, c, testNdb, ns)
+			_, _, err := k8sutils.Exec(c, mysqlPod0Name, ns, mysqlCmd)
+			ndbtest.ExpectNoError(err, "Failure executing command %s", mysqlCmd)
 
-			_, _, err := k8sutils.Exec(c, mysqlpodlist.Items[0].Name, ns, cmd)
-			ndbtest.ExpectNoError(err, "Failure executing command %s", cmd)
-
-			_, _, err = k8sutils.Exec(c, mysqlpodlist.Items[1].Name, ns, cmd)
+			_, _, err = k8sutils.Exec(c, mysqlPod1Name, ns, mysqlCmd)
 			ndbtest.ExpectError(err, "Expecting failure but succeed")
 		})
 	})
@@ -134,47 +117,12 @@ var _ = ndbtest.NewOrderedTestCase("Mutable root host", func(tc *ndbtest.TestCon
 		})
 
 		ginkgo.It("ensure that root user is accessible from all pod", func() {
-			cmd := buildExecCmd(ctx, c, testNdb, ns)
+			_, _, err := k8sutils.Exec(c, mysqlPod0Name, ns, mysqlCmd)
+			ndbtest.ExpectNoError(err, "Failure executing command %s", mysqlCmd)
 
-			_, _, err := k8sutils.Exec(c, mysqlpodlist.Items[0].Name, ns, cmd)
-			ndbtest.ExpectNoError(err, "Failure executing command %s", cmd)
-
-			_, _, err = k8sutils.Exec(c, mysqlpodlist.Items[1].Name, ns, cmd)
-			ndbtest.ExpectNoError(err, "Failure executing command %s", cmd)
+			_, _, err = k8sutils.Exec(c, mysqlPod1Name, ns, mysqlCmd)
+			ndbtest.ExpectNoError(err, "Failure executing command %s", mysqlCmd)
 		})
 
-	})
-
-	//CASE 4: Scaledown and Scaleup mysqld with RootHost = unknown ip
-	ginkgo.When("MySQL Servers are scaled down to 0 and scaled up again with RootHost = unknown ip", func() {
-
-		ginkgo.BeforeAll(func() {
-			//Scale down
-			testNdb.Spec.Mysqld = nil
-			ndbtest.KubectlApplyNdbObjNoWait(testNdb)
-			// validate the status updates made by the operator during the sync
-			ndbutils.ValidateNdbClusterStatusUpdatesDuringSync(
-				ctx, tc.NdbClientset(), ns, ndbName)
-			mysqlpodlist = nil
-
-			//Scale up
-			testNdb.Spec.Mysqld = &v1alpha1.NdbMysqldSpec{
-				RootHost:  "1.1.0.1",
-				NodeCount: 2,
-			}
-		})
-
-		ginkgo.It("ensure that root user is not accessible from any of the mysql pod in cluster", func() {
-			testNdb.Spec.Mysqld = &v1alpha1.NdbMysqldSpec{
-				NodeCount: 3,
-			}
-			cmd := buildExecCmd(ctx, c, testNdb, ns)
-
-			_, _, err := k8sutils.Exec(c, mysqlpodlist.Items[0].Name, ns, cmd)
-			ndbtest.ExpectError(err, "Expecting failure but succeed")
-
-			_, _, err = k8sutils.Exec(c, mysqlpodlist.Items[1].Name, ns, cmd)
-			ndbtest.ExpectError(err, "Expecting failure but succeed")
-		})
 	})
 })
---
+++ pkg/webhook/ndb_admission_controller_test.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2022, Oracle and/or its affiliates.
+//
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+package webhook
+
+import (
+	"github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1alpha1"
+	"github.com/mysql/ndb-operator/pkg/helpers/testutils"
+	"testing"
+)
+
+func Test_ndbAdmissionController_mutate(t *testing.T) {
+	type mutatorTestCases struct {
+		desc          string
+		ncSpec        *v1alpha1.NdbClusterSpec
+		expectedPatch string
+	}
+
+	testcases := []mutatorTestCases{
+		{
+			desc: "mysqldSpec is nil",
+			ncSpec: &v1alpha1.NdbClusterSpec{
+				Mysqld: nil,
+			},
+			expectedPatch: `[{"op":"add","path":"/spec/mysqld","value":{"nodeCount":1}}]`,
+		},
+		{
+			desc: "mysqldSpec nodeCount is 0",
+			ncSpec: &v1alpha1.NdbClusterSpec{
+				Mysqld: &v1alpha1.NdbMysqldSpec{
+					NodeCount: 0,
+				},
+			},
+			expectedPatch: `[{"op":"replace","path":"/spec/mysqld/nodeCount","value":1}]`,
+		},
+		{
+			desc: "mysqldSpec nodeCount is 1",
+			ncSpec: &v1alpha1.NdbClusterSpec{
+				Mysqld: &v1alpha1.NdbMysqldSpec{
+					NodeCount: 1,
+				},
+			},
+			// No patch expected
+		},
+	}
+
+	ndbAc := newNdbAdmissionController()
+	nc := testutils.NewTestNdb("default", "test", 1)
+	for _, tc := range testcases {
+		nc.Spec = *tc.ncSpec
+		originalPatch, err := ndbAc.mutate(nc)
+		if err != nil {
+			t.Errorf("Testcase %q failed with error %q", tc.desc, err)
+			continue
+		}
+
+		if string(originalPatch) != tc.expectedPatch {
+			t.Errorf("Testcase %q failed : Expected patch `%s` but got `%s`", tc.desc, tc.expectedPatch, string(originalPatch))
+		}
+	}
+}
