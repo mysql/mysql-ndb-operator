0a0f1b0f1a8cf079dfd5554c9efbb26e0ac50aad
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2022-08-16 01:19:52 +0530

    MYSQLCLUSTER-5951 Ensure atleast one MySQL Server is always run [2/3]
    Added a mutating admission controller for NdbCluster.
    
    Change-Id: I399f3189666efa32e5b309be35798a1290c0374e

--- pkg/controllers/webhookconfig_controller.go
+++ pkg/controllers/webhookconfig_controller.go
@@ -33,6 +33,18 @@ func NewValidatingWebhookConfigController(client k8s.Interface) WebhookConfigCon
 	}
 }
 
+// mutatingWebhookConfigController implements WebhookConfigController for MutatingWebhookConfigs
+type mutatingWebhookConfigController struct {
+	mwcInterface typedadmissionregistrationv1.MutatingWebhookConfigurationInterface
+}
+
+// NewMutatingWebhookConfigController creates and returns a new mutatingWebhookConfigController
+func NewMutatingWebhookConfigController(client k8s.Interface) WebhookConfigController {
+	return &mutatingWebhookConfigController{
+		mwcInterface: client.AdmissionregistrationV1().MutatingWebhookConfigurations(),
+	}
+}
+
 // UpdateWebhookConfigCertificate updates the webhooks with the given TLS certificate data
 func (c *validatingWebhookConfigController) UpdateWebhookConfigCertificate(
 	ctx context.Context, labelSelector string, cert []byte) bool {
@@ -82,6 +94,60 @@ func (c *validatingWebhookConfigController) UpdateWebhookConfigCertificate(
 		}
 
 		klog.Infof("Successfully updated the validatingWebhookConfig '%s' with the certificate", newVwc.Name)
+	}
+
+	return true
+}
+
+// UpdateWebhookConfigCertificate updates the webhooks with the given TLS certificate data
+func (c *mutatingWebhookConfigController) UpdateWebhookConfigCertificate(
+	ctx context.Context, labelSelector string, cert []byte) bool {
+	// Get all validating configs with matching label
+	mwcList, err := c.mwcInterface.List(ctx, metav1.ListOptions{
+		LabelSelector: labelSelector,
+	})
+	if err != nil {
+		klog.Errorf("Failed to retrieve mutating webhook config list : %s", err)
+		return false
+	}
+
+	// Update them one by one
+	for _, mwc := range mwcList.Items {
+		// Make a copy of the webhook config to update
+		newMwc := mwc.DeepCopy()
+
+		// Update all webhooks' CA bundle
+		for i := range newMwc.Webhooks {
+			newMwc.Webhooks[i].ClientConfig.CABundle = cert
+		}
+
+		// Prepare a patch to be applied
+		existingJSON, err := json.Marshal(mwc)
+		if err != nil {
+			klog.Error("Failed to encode existing mutating webhook config : ", err)
+			return false
+		}
+		updatedJSON, err := json.Marshal(newMwc)
+		if err != nil {
+			klog.Error("Failed to encode updated mutating webhook config : ", err)
+			return false
+		}
+		patch, err := strategicpatch.CreateTwoWayMergePatch(
+			existingJSON, updatedJSON, admissionregistrationv1.MutatingWebhookConfiguration{})
+		if err != nil {
+			klog.Error("Failed to generate the patch to be applied : ", err)
+			return false
+		}
+
+		// Apply the patch
+		if newMwc, err = c.mwcInterface.Patch(
+			ctx, newMwc.Name, types.StrategicMergePatchType, patch, metav1.PatchOptions{}); err != nil {
+			klog.Errorf("Failed to patch mutating webhook config %s : %s",
+				newMwc.Name, err)
+			return false
+		}
+
+		klog.Infof("Successfully updated the mutatingWebhookConfig '%s' with the certificate", newMwc.Name)
 	}
 
 	return true
