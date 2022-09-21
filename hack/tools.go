65bccbb53aac1bdcf6cc5c610b3b353f60950821
Lakshmi Narayanan Sreethar <lakshmi.narayanan.sreethar@oracle.com>
2022-09-21 14:34:10 +0530

    Update the e2e tests to use the vendor directory
    Change-Id: Ie55ffe1b81c38e482f7a5920eb4e69445c6c5cfc

--- hack/tools.go
+++ hack/tools.go
@@ -5,11 +5,13 @@
 //
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies
+// This package imports additional libraries required by build scripts
+// and test files, to force `go mod` to see them as dependencies.
 package tools
 
 import (
 	_ "github.com/onsi/ginkgo/v2/ginkgo"
+	_ "k8s.io/apimachinery/pkg/util/uuid"
 	_ "k8s.io/code-generator"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kind"
