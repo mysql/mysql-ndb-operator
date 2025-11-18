bd4fcdbcf1094d42b7c71a94584bd89b74addeaa
Paulo Silva <paulo.p.silva@oracle.com>
2025-11-18 11:46:02 +0000

    MYSQLCLUSTER-9586 Update base images and dependencies
    The Go version is bumped to 1.25.4, and all Go libraries required
    by the NDB Operator are updated to their latest available version. Base
    images, installed tools and KinD images used in tests are also updated.
    
    Change-Id: I4a5140189ba205b00098b7868b05f9e367fb5d11

--- e2e-tests/run-e2e-test.go
+++ e2e-tests/run-e2e-test.go
@@ -65,9 +65,10 @@ var kindK8sNodeImages = map[string]string{
 	"1.28": "kindest/node:v1.28.15@sha256:a7c05c7ae043a0b8c818f5a06188bc2c4098f6cb59ca7d1856df00375d839251",
 	"1.29": "kindest/node:v1.29.14@sha256:8703bd94ee24e51b778d5556ae310c6c0fa67d761fae6379c8e0bb480e6fea29",
 	"1.30": "kindest/node:v1.30.13@sha256:397209b3d947d154f6641f2d0ce8d473732bd91c87d9575ade99049aa33cd648",
-	"1.31": "kindest/node:v1.31.9@sha256:b94a3a6c06198d17f59cca8c6f486236fa05e2fb359cbd75dabbfc348a10b211",
-	"1.32": "kindest/node:v1.32.5@sha256:e3b2327e3a5ab8c76f5ece68936e4cafaa82edf58486b769727ab0b3b97a5b0d",
-	"1.33": "kindest/node:v1.33.1@sha256:050072256b9a903bd914c0b2866828150cb229cea0efe5892e2b644d5dd3b34f",
+	"1.31": "kindest/node:v1.31.12@sha256:0f5cc49c5e73c0c2bb6e2df56e7df189240d83cf94edfa30946482eb08ec57d2",
+	"1.32": "kindest/node:v1.32.8@sha256:abd489f042d2b644e2d033f5c2d900bc707798d075e8186cb65e3f1367a9d5a1",
+	"1.33": "kindest/node:v1.33.4@sha256:25a6018e48dfcaee478f4a59af81157a437f15e6e140bf103f85a2e7cd0cbbf2",
+	"1.34": "kindest/node:v1.34.0@sha256:7416a61b42b1662ca6ca89f02028ac133a309a2a30ba309614e8ec94d976dc5a",
 }
 
 var (
@@ -889,7 +890,7 @@ func init() {
 			"By default, this is not enabled and the tests will be run as a pod from inside K8s Cluster.")
 
 	// use v1.33 as default kind k8s version
-	flag.StringVar(&options.kindK8sVersion, "kind-k8s-version", "1.33",
+	flag.StringVar(&options.kindK8sVersion, "kind-k8s-version", "1.34",
 		"Kind k8s version used to run tests.\n"+
 			"Example usage: -kind-k8s-version=1.30")
 
