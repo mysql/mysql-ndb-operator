85922f05473b6acfd5db123cde0e8c666998abd5
Paulo Silva <paulo.p.silva@oracle.com>
2026-03-25 15:50:47 +0000

    Bump NDB Operator versions on 'main-8.4' branch
    Bump version to 8.4.9 in the below files:
    - VERSION file
    - ndb-operator chart files
    - types.go
    - NdbCluster-CRD.md
    - LICENSE file
    
    Approved by: Luis Agostinho <luis.agostinho@oracle.com>
    
    Change-Id: Ie1e3e0a22c91251dee43be40d994dbd888ac67f3

--- pkg/apis/ndbcontroller/v1/types.go
+++ pkg/apis/ndbcontroller/v1/types.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, 2025, Oracle and/or its affiliates.
+// Copyright (c) 2020, 2026, Oracle and/or its affiliates.
 //
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
 
@@ -236,8 +236,8 @@ type NdbClusterSpec struct {
 	// +optional
 	TDESecretName string `json:"tdeSecretName,omitempty"`
 	// The name of the MySQL Ndb Cluster image to be used.
-	// If not specified, "container-registry.oracle.com/mysql/community-cluster:8.4.8" will be used.
-	// +kubebuilder:default="container-registry.oracle.com/mysql/community-cluster:8.4.8"
+	// If not specified, "container-registry.oracle.com/mysql/community-cluster:8.4.9" will be used.
+	// +kubebuilder:default="container-registry.oracle.com/mysql/community-cluster:8.4.9"
 	// +optional
 	Image string `json:"image,omitempty"`
 	// ImagePullPolicy describes a policy for if/when to
